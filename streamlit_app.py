d55ad46e2e2543c078057274588fd5bddb457f9c
lex7sss <alexisjuma14@gmail.com>
2025-05-29 11:23:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,5 @@ import openai
 
 st.set_page_config(page_title="Mood2Fit")
 st.title("Mood2Fit- Your Ai stylist")
-st.write(Get outfit inspiration based on your **mood**, **event**, or **song**. ")
+st.write("Get outfit inspiration based on your **mood**, **event**, or **song**.")
+
