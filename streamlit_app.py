7923ed853d334379020676dee51c6cf1fef50bf7
lex7sss <alexisjuma14@gmail.com>
2025-05-29 11:37:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,2 @@
-import streamlit as st
-requirements.txt
-steamlit
-openai
-import openai
-st.set_page_config(page_title="Mood2Fit")
-st.title("Mood2Fit- Your Ai stylist")
-st.write("Get outfit inspiration based on your **mood**, **event**, or **song**.")
+pip install streamlit openai
 
